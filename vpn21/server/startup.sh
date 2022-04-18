a4f72fca3f2964911e0a8d5fcffedbe782561bb6
marc <marc@pcmarc.pcmarc>
2022-04-18 12:32:20 +0200

    m

--- vpn21/server/startup.sh
+++ vpn21/server/startup.sh
@@ -2,7 +2,8 @@
 
 cp -r . /etc/openvpn/server
 mkdir -p /usr/lib/systemd/system
-cp /opt/docker/openvpn@.service /usr/lib/systemd/system/openvpn@.service
+
+#cp /opt/docker/openvpn@.service /usr/lib/systemd/system/openvpn@.service # NO SE ON VA SI SERVIDOR O HOST DEL DOCKER
 
 systemctl start openvpn-server@server.service
 
